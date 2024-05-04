4512c937e9659e57d1f30d1e7f32550a753bfa80
Babu rao sanivada <baburao.sanivada10@gmail.com>
2024-05-04 17:03:37 +0530

    get WishList Items Done

--- src/main/java/com/Ecommerce/ShopSphere/Controller/WishListController.java
+++ src/main/java/com/Ecommerce/ShopSphere/Controller/WishListController.java
@@ -1,13 +1,17 @@
 package com.Ecommerce.ShopSphere.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Ecommerce.ShopSphere.Common.ApiResponse;
+import com.Ecommerce.ShopSphere.DTO.ProductDto;
 import com.Ecommerce.ShopSphere.Model.Product;
 import com.Ecommerce.ShopSphere.Model.User;
 import com.Ecommerce.ShopSphere.Service.TokenService;
@@ -18,6 +22,9 @@ import springfox.documentation.swagger.web.ApiResourceController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
@@ -32,7 +39,7 @@ public class WishListController {
 
     //Add item to wishList
     @PostMapping("/add")
-    public ResponseEntity<ApiResponse> AddWishListItem(@RequestParam("token") String token,@RequestBody Product product) {
+    public ResponseEntity<ApiResponse> AddWishListItem(@RequestHeader("token") String token,@RequestBody Product product) {
         //Authorize the token
         tokenService.authenticateUser(token);
 
@@ -44,6 +51,23 @@ public class WishListController {
 
         return new ResponseEntity<>(new ApiResponse(true, "Added Item to wishList"),HttpStatus.OK);
     }
+
+    //Get all WishList items
+    @GetMapping("/items/{token}")
+    public ResponseEntity<List<ProductDto>> getWishListItems(@PathVariable("token") String token) {
+
+        //Authorize the token
+        tokenService.authenticateUser(token);
+
+        //Get the user
+        User user = tokenService.getUserWithToken(token);
+
+        List<ProductDto> products=wishListService.getWishListItems(user);
+
+        return new ResponseEntity<List<ProductDto>>(products, HttpStatus.OK);
+
+    }
+    
     
     
 }
--- src/main/java/com/Ecommerce/ShopSphere/Service/WishListService.java
+++ src/main/java/com/Ecommerce/ShopSphere/Service/WishListService.java
@@ -1,8 +1,12 @@
 package com.Ecommerce.ShopSphere.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Ecommerce.ShopSphere.DTO.ProductDto;
 import com.Ecommerce.ShopSphere.Model.Product;
 import com.Ecommerce.ShopSphere.Model.User;
 import com.Ecommerce.ShopSphere.Model.WishList;
@@ -14,8 +18,20 @@ public class WishListService {
     @Autowired
     private WishListRepo wishListRepo;
 
+    @Autowired
+    private ProductService productService;
+
     public void AddItemToWishList(User user,Product product){
         WishList instance = new WishList(user, product);
         wishListRepo.save(instance);
+    }
+
+    public List<ProductDto> getWishListItems(User user) {
+       List<WishList> wishListItems=wishListRepo.findAllByUserOrderByCreatedDateDesc(user);
+       List<ProductDto> productDtos = new ArrayList<>();
+       for(WishList item:wishListItems){
+        productDtos.add(productService.getProductDto(item.getProduct()));
+       }
+       return productDtos;
     }
 }
